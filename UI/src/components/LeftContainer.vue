d68a3dc5abd247db9f60df95eca65e16a03127f8
Victor-Bertalan <victorbertalan@gmail.com>
2022-05-23 18:17:15 +0300

    add left container

---
+++ UI/src/components/LeftContainer.vue
@@ -0,0 +1,47 @@
+
+<script>
+import Info from './Info.vue'
+import Statistics from './Statistics.vue'
+import History from './History.vue'
+export default {
+    data(){
+        return {}
+    },
+    components: {
+        Info,
+        Statistics,
+        History
+    }
+}
+</script>
+
+<template>
+    <div id="container">
+        <Info id="info"/>
+        <Statistics id="stats"/>
+        <History id="history"/>
+    </div>
+</template>
+
+<style scoped>
+#container{
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    height: 100vh;
+    width: 33vw;
+    background-color: #003049;
+}
+#info{
+    width: 100%;
+    height: 20%; 
+}
+#stats{
+    width:100%;
+    height:  35%;
+}
+#history{
+    width:100%;
+    height: 45%;
+}
+</style>
